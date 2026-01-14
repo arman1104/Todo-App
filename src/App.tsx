e707e1e9028f3e27e9ca6797267befde6336454c
arman1104 <arman051215@gmail.com>
2026-01-14 12:04:44 +0530

    worked on App.tsx and Header.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-// import React from "react";
+import bgDesktopDark from "./assets/images/bg-desktop-dark.jpg";
+import Header from "./components/Header";
+// import bgDesktopLight from "./assets/images/bg-desktop-light.jpg"
+// import bgMobileDark from "./assets/images/bg-mobile-dark.jpg"
+// import bgMobileLight from "./assets/images/bg-mobile-light.jpg"
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
+    <div className="min-h-screen bg-[#181824]">
+      {/* Background Section */}
+      <div className="relative h-[300px] w-full">
+        <img
+          src={bgDesktopDark}
+          alt="bg-desktop-dark"
+          className="h-full w-full object-cover"
+        />
+
+        {/* Header overlay */}
+        <div className="absolute inset-0">
+          <Header />
+        </div>
+      </div>
     </div>
   );
 };
